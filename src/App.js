aeab036c796e726fda222084248a6696e81173f4
Mark Lunney <mark.lunney@freesat.co.uk>
2017-09-25 16:46:48 +0100

    Updated to use create-react-library

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Menu } from 'create-react-styleguist/styleguide/build/bundle.4b6e7d4a.js';
-//import { Menu } from 'create-react-styleguist/styleguide/build/0.08da7a36.js';
+import { Example, SecondExample }  from 'create-react-library';
 
 
 class App extends Component {
@@ -15,7 +14,8 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          <Menu />
+          <Example title="Hello World Test" />
+          <SecondExample />
         </p>
       </div>
     );
